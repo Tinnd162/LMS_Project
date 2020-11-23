44a755c36304ccc51d000240c0277cd05aee0d2b
LamHV <lamhv155@gmail.com>
2020-11-23 11:05:44 +0700

    add menusidebar of Teacher area

---
+++ DAL/DAO/CourseDAO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.EF;
+
+namespace DAL.DAO
+{
+    public class CourseDAO
+    {
+        LMSProjectDBContext db = null;
+        
+        public CourseDAO() { db = new LMSProjectDBContext(); }
+        
+        public List<COURSE> GetCOURSEs()
+        {
+            return db.COURSEs.ToList();
+        }
+    }
+}
---
+++ DAL/DAO/SubjectDAO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.EF;
+
+namespace DAL.DAO
+{
+    public class SubjectDAO
+    {
+        LMSProjectDBContext db = null;
+        public SubjectDAO() { db = new LMSProjectDBContext(); }
+
+        public List<SUBJECT> GetSubjectByTeacherAndCourse(int teacher_id, int course_id)
+        {
+            object[] sqlParams=
+            {
+                new SqlParameter("@user_id", teacher_id),
+                new SqlParameter("@course_id", course_id)
+            };
+            var listSubject = db.Database.SqlQuery<SUBJECT>("GET_SUBJECT_BY_TEACHER_AND_COURSE @user_id, @course_id", sqlParams).ToList();
+            return listSubject;
+        }
+    }
+}
