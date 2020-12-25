8ac45c355ecf6844c493dca58da7e02c479dd3be
vtp1201 <tiachopdo1201@gmail.com>
2020-12-25 13:13:05 +0700

    add submit detail student

--- LMS/Areas/Student/Controllers/HomeController.cs
+++ LMS/Areas/Student/Controllers/HomeController.cs
@@ -14,19 +14,12 @@ namespace LMS.Areas.Student.Controllers
         // GET: Student/Home
         //public ActionResult Index()
         //{
-        //    return View(); 
+        //    return View();
         //}
         public ViewResult Index(string searchString, string user_id = "U00008", string semester_id = "20211")
         {
             SubmitDAO dao = new SubmitDAO();
             List<COURSE> courses = dao.GetDeadlinebyStuAndCourseAndSem(user_id, semester_id);
-            //var listDeadLine = from s in Deadline.GetDeadlinebyStuAndCourseAndSem(user_id, semester_id) select s;
-            //if (!String.IsNullOrEmpty(searchString))
-            //{
-            //    listDeadLine = listDeadLine.Where(s => s.submitID.Contains(null));
-            //}
-            //listDeadLine = listDeadLine.OrderBy(s => s.eventDeadline);
-            //return View(listDeadLine.ToList());
             List<DeadlineView> dView = new List<DeadlineView>();
 
             foreach(COURSE course in courses)
@@ -61,13 +54,6 @@ namespace LMS.Areas.Student.Controllers
             ListEvent = ListEvent.OrderBy(s => s.eventDeadline);
             return View(ListEvent.ToList());
 
-        }   
-
-        //public ActionResult Index(string user_id = "U00008", string semester_id = "20211")
-        //{
-        //    SubmitDAO Deadline = new SubmitDAO();
-        //    var listDeadLine = Deadline.GetDeadlinebyStuAndCourseAndSem(user_id, semester_id);
-        //    return View(listDeadLine);
-        //}
+        }  
     }
 }
